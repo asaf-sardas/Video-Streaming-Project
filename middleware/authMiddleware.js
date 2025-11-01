b1ac37e480d231f4e439b8d239c4c94db1ac8f7d
asaf-sardas <asafsardas1@gmail.com>
2025-11-01 19:31:21 +0200

    added authentication and admin verify middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+//check if the user is authenticated
+export const isAuthenticated = (req, res, next) => {
+  if (req.session && req.session.user) {
+    return next();
+  } else {
+    return res.status(401).json({
+      error: "Unauthorized: You must be logged in to access this resource.",
+    });
+  }
+};
+
+//check if the user is admin
+export const isAdmin = (req, res, next) => {
+  if (req.session.user.role === "admin") {
+    return next();
+  } else {
+    return res
+      .status(403)
+      .json({ error: "Forbidden: Access denied. Admin permissions required." });
+  }
+};
