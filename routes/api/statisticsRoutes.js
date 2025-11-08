658acac6ead2c05f82b4bab1574831849e04f544
asaf-sardas <asafsardas1@gmail.com>
2025-11-09 00:26:09 +0200

    created daily  views chart

--- routes/api/statisticsRoutes.js
+++ routes/api/statisticsRoutes.js
@@ -4,5 +4,6 @@ const statisticsController = require("../../controllers/statisticsController");
 
 // Basic genre routes
 router.route("/genre").get(statisticsController.sumPopularityByGenre); // Get all genres popularties
+router.route("/profile-views/:userId").get(statisticsController.dailyViews); // Get views by profile
 
 module.exports = router;
